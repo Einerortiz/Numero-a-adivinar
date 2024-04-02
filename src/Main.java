77de088b4aa3a782f6a6b8a6a4282f5e9e9cbb4d
Einerortiz <146882886+Einerortiz@users.noreply.github.com>
2024-04-02 14:34:40 -0500

    :)

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+        int numeroAadivinar = random.nextInt(100);
+        int intentosf = 0;
+
+        System.out.println("Bienvenido al juego de adivinar el número :D");
+
+        int Adivinar;
+        do {
+            System.out.print("Introduce tu numero :) : ");
+            Adivinar = input.nextInt();
+            intentosf++;
+
+            if (Adivinar < numeroAadivinar) {
+                System.out.println("El número a adivinar es mayor.");
+            } else if (Adivinar > numeroAadivinar) {
+                System.out.println("El número a adivinar es menor.");
+            }
+        } while (Adivinar != numeroAadivinar);
+
+        System.out.println("Felicidades Adivinaste el número en " + intentosf + " intentos fallidos.");
+    }
+}
